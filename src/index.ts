ccdca87e5b28e683d21ebde36fac46efae022ec6
Thomas Ghysels <info@thomasg.be>
2023-06-09 15:30:12 +0200

    Refactor to router based implementation

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,61 @@
 import { Config } from 'payload/config'
-import { useDocumentInfo } from 'payload/components/utilities'
 import { useLayoutEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export function bodyClassname(config: Config): Config {
   return {
     ...config,
-    collections: config.collections?.map((collection) => ({
-      ...collection,
-      fields: collection.fields.concat({
-        type: 'ui',
-        name: 'bodyClassname',
-        admin: {
-          components: {
-            Field: BodyClassname,
-            Cell: BodyClassname,
-          },
-        },
-      }),
-    })),
+    admin: {
+      ...config.admin,
+      components: {
+        ...config.admin?.components,
+        afterNavLinks: [
+          ...(config.admin?.components?.afterNavLinks || []),
+          useBodyClassname,
+        ],
+      },
+    },
   }
 }
 
-function BodyClassname() {
-  // @ts-expect-error Slug is missing in types
-  const { type, slug } = useDocumentInfo()
+let staleClass = ''
+
+// It's a hook and also a Component!
+export function useBodyClassname() {
+  const loc = useLocation()
+
   useLayoutEffect(() => {
-    if (!type || !slug) return
-    document.body.classList.add(type + '-' + slug)
-    return () => document.body.classList.remove(type + '-' + slug)
-  }, [slug])
+    // Start from previous value
+    let beforeClass = staleClass
+    staleClass = ''
+
+    // Parse the new URL
+    let [, _, collections, next] = loc.pathname.split('/')
+    next =
+      collections === 'collections' || collections === 'globals'
+        ? collections.slice(0, -1) + '-' + next
+        : ''
+    console.log('body', staleClass, beforeClass, next)
+
+    // Keep as is
+    if (beforeClass !== next) {
+      // Remove the previous class name
+      if (beforeClass) document.body.classList.remove(beforeClass)
+      if (next) document.body.classList.add(next)
+    }
+
+    if (next)
+      return () => {
+        staleClass = next
+
+        // Delayed cleanup
+        setTimeout(() => {
+          if (staleClass) {
+            document.body.classList.remove(staleClass)
+            staleClass = ''
+          }
+        }, 200) as unknown as number
+      }
+  }, [loc.pathname])
   return null
 }
